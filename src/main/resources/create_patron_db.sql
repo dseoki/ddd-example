cbb8178f00656a8c11ad6b265d8c3eeae5f9aa3d
dseoki <jeon931207@naver.com>
2023-07-06 10:04:43 +0900

    설정 및 최초 구동 확인

--- src/main/resources/create_patron_db.sql
+++ src/main/resources/create_patron_db.sql
@@ -3,3 +3,5 @@ CREATE TABLE IF NOT EXISTS patron_database_entity (id INTEGER IDENTITY PRIMARY K
 CREATE TABLE IF NOT EXISTS hold_database_entity (id INTEGER IDENTITY PRIMARY KEY, book_id UUID NOT NULL, patron_id UUID NOT NULL, library_branch_id UUID NOT NULL, patron_database_entity INTEGER NOT NULL, till TIMESTAMP NOT NULL);
 
 CREATE TABLE IF NOT EXISTS overdue_checkout_database_entity (id INTEGER IDENTITY PRIMARY KEY, book_id UUID NOT NULL, patron_id UUID NOT NULL, library_branch_id UUID NOT NULL, patron_database_entity INTEGER NOT NULL);
+
+INSERT INTO patron_database_entity (patron_type, patron_id) VALUES ('some_type', 'b7f9d634-2635-4adf-9b7a-8f91b6e1a3cd');
